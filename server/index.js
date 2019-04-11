58a62fae088d10b04eafcad256dcbbb77924876a
jyoti kumra <jo.kumra@gmail.com>
2019-04-10 22:39:12 -0700

    Complete proxy with all the components loading properly

--- server/index.js
+++ server/index.js
@@ -15,13 +15,45 @@ app.get('/house_images', (req,res) => {
    request(images_url, { json: true }, (error, response, body) => {
      if (error) { return console.log(error); }
      //console.log(body);
-     console.log(body.results);
-     res.send({results:body.results});
+     let results = body.results
+     console.log(results);
+     res.send({results});
    });
 
-
-
-
 })
+
+app.get('/description', (req, res) => {
+  let id= req.query._id;
+  let description_url = "http://localhost:3210/description?_id=" + id;
+  request(description_url, { json: true }, (error, response, body) => {
+    if (error) { return console.log(error); }
+    //console.log(body);
+    console.log(body);
+    res.send(body);
+  });
+});
+
+app.get('/morehomes', (req, res) => {
+  let id= req.query.id;
+  let morehome_url = "http://localhost:3000/morehomes";
+  request(morehome_url, { json: true }, (error, response, body) => {
+    if (error) { return console.log(error); }
+    //console.log(body);
+    console.log(body);
+    res.send(body);
+  });
+});
+
+app.get('/totalReviews', (req, res) => {
+  let id= req.query.id;
+  let reviews_url = "http://localhost:3004/totalReviews?id=" + id;
+  request(reviews_url, { json: true }, (error, response, body) => {
+    if (error) { return console.log(error); }
+    //console.log(body);
+    console.log(body);
+    res.send(body);
+  });
+});
+
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
